2a2be30dcdc2f0892baec58b816ff59c00ba037c
furyy-j <96385920+furyy-j@users.noreply.github.com>
2022-10-17 19:55:59 +0300

    create mission manifest models

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
+import { MissionComponent } from './mission/mission.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'mission',
+  },
+  {
+    path: 'mission',
+    component: MissionComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
